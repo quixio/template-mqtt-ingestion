fa81336a6496ca996091c6123e2812bffe91862b
Tim Sawicki <tim@quix.io>
2025-07-29 14:43:43 -0400

    update MQTT normalizer

--- mqtt-data-normalization/main.py
+++ mqtt-data-normalization/main.py
@@ -9,13 +9,31 @@ load_dotenv()
 
 def expand_key(row, key, timestamp, headers):
     expanded_key = key.split("/")
-    result = {
+    return {
         "machine": expanded_key[-2],
         "sensor": expanded_key[-1],
-        "timestamp": timestamp
+        "value": float(bytes.decode(row)),
+        "timestamp": timestamp,
     }
-    result["value"] = bytes.decode(row)
-    return result
+
+
+def window_initializer(row: dict) -> dict:
+    return {"machine": row["machine"]}
+
+
+def window_reducer(agg: dict, row: dict) -> dict:
+    agg.setdefault(row["sensor"], []).append(row["value"])
+    return agg
+
+
+def window_finalizer(finalized_window: dict):
+    agg_values = finalized_window["value"]
+    machine = agg_values.pop("machine")
+    return {
+        **{k: round(sum(v) / len(v), 2) for k, v in agg_values.items()},
+        "timestamp": str(datetime.fromtimestamp(finalized_window["start"] / 1000)),
+        "machine": machine
+    }
 
 
 app = Application(
@@ -26,17 +44,13 @@ output_topic = app.topic(os.environ["output"])
 sdf = app.dataframe(input_topic)
 
 sdf = sdf.apply(expand_key, metadata=True)
+sdf.update(lambda row: print('PRIOR TO TIMESTAMP')).print(metadata=True)
 sdf = sdf.set_timestamp(lambda row, *_: row["timestamp"])
 sdf = sdf.drop("timestamp")
 sdf = sdf.group_by("machine")
-sdf = sdf.apply(
-    lambda row: {row["sensor"]: row["value"], "machine": row["machine"]}
-)
-sdf = sdf.hopping_window(10000, 2000).reduce(lambda window, row: {**window, **row}, lambda row: row).final()
-sdf = sdf.apply(lambda row: {
-    **row["value"],
-    "timestamp": str(datetime.fromtimestamp(row["start"] / 1000))
-})
+sdf.update(lambda row: print('AFTER GROUPBY')).print(metadata=True)
+sdf = sdf.hopping_window(1000, 200, 500).reduce(reducer=window_reducer, initializer=window_initializer).final()
+sdf = sdf.apply(window_finalizer)
 sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
